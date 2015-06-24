8ab91a8cd767cb9da0e316675aea9adb61cab017
hanbyul-here <hanbyul@nyu.edu>
2015-06-24 18:12:00 -0400

    first commit for lrm-valhalla

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,26 @@
-var map = L.map('map');
+var map = L.map('map',{
+  inertia: false
+});
 
+// Using Tangram 
+var layer = Tangram.leafletLayer({
+  scene: 'scene.yaml',
+        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
+});
+layer.addTo(map);
+
+/*
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
+*/
 L.Routing.control({
 	waypoints: [
 		L.latLng(57.74, 11.94),
 		L.latLng(57.6792, 11.949)
 	],
-	geocoder: L.Control.Geocoder.nominatim(),
-	router: L.Routing.graphHopper('your-api-key'),
-	routeWhileDragging: false
+	router: L.Routing.valhalla('valhalla-3IMakjA','auto'),
+  formatter: new L.Routing.Valhalla.Formatter(),
+  summaryTemplate:'<div class="start">{name}</div><div class="info {transitmode}">{distance}, {time}</div>',
+  routeWhileDragging: false
 }).addTo(map);
