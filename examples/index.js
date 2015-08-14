fec391769fc388ba9f7dbe1bc09c54cb7f94132e
hanbyul-here <hanbyul@nyu.edu>
2015-08-14 14:29:43 -0400

    wow typo

--- examples/index.js
+++ examples/index.js
@@ -19,7 +19,7 @@ L.Routing.control({
     L.latLng(36.4156136, -119.849234)
   ],
   // You can get your own Valhalla API key from the Mapzen developer portal (https://mapzen.com/developers/)
-  router: L.Routing.valhalla('<my apiey>', 'auto'),
+  router: L.Routing.valhalla('<my api key>', 'auto'),
   formatter: new L.Routing.Valhalla.Formatter(),
   summaryTemplate:'<div class="start">{name}</div><div class="info {transitmode}">{distance}, {time}</div>'
 }).addTo(map);
