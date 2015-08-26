6d6a936e3416666fc128f190f79acff3231e4049
hanbyul-here <hanbyul@nyu.edu>
2015-08-26 11:06:31 -0400

    it only should happen when unit is mile

--- src/L.Routing.Valhalla.Formatter.js
+++ src/L.Routing.Valhalla.Formatter.js
@@ -31,10 +31,9 @@
       var un = this.options.unitNames,
           v,
         data;
-
+      if (this.options.units === 'imperial') {
         //valhalla returns distance in km 
         d  = d * 1000;
-      if (this.options.units === 'imperial') {
         d = d / 1.609344;
         if (d >= 1000) {
           data = {
