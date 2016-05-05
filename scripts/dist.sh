355ed6bdbb9462de8014a6fcb93494f8f33fd60c
Hanbyul Jo <hanbyul@admins-MacBook-Pro-4.local>
2016-05-05 12:13:04 -0400

    optimizing & fixing error before releasing 1.0

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -5,7 +5,7 @@ VERSION=`echo "console.log(require('./package.json').version)" | node`
 echo Building dist files for $VERSION...
 mkdir -p dist
 browserify -t browserify-shim src/* > dist/lrm-mapzen.js
-uglifyjs dist/lrm-mapzen.js > dist/lrm-mapzen.min.js
+browserify -t uglifyify -t browserify-shim src/* | uglifyjs -c > dist/lrm-mapzen.min.js
 
 cp css/leaflet.routing.mapzen.css dist/leaflet.routing.mapzen.css
 cp css/leaflet.routing.icons.svg dist/leaflet.routing.icons.svg
