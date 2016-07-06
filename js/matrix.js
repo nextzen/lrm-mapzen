366d17918f88111cdad1af87b9e7cd94b0fa2870
Hanbyul Jo <hanbyul@nyu.edu>
2016-07-06 15:44:55 -0400

    adding optimized routing example with patched version of lrm for internal purpose

---
+++ js/matrix.js
@@ -0,0 +1,42 @@
+var map = L.map('map');
+
+
+if (hasWebGL()) {
+  // use Tangram to draw tiles when there is WebGL available on the browser
+  var layer = Tangram.leafletLayer({
+    scene: 'https://cdn.rawgit.com/tangrams/refill-style/gh-pages/refill-style.yaml',
+    attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
+  });
+  layer.addTo(map);
+} else {
+  // Use normal OSM tiles instead of Tangram when there is no webgl available
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+   }).addTo(map);
+}
+
+
+// detect webgl on browser for Tangram
+function hasWebGL() {
+  try {
+    var canvas = document.createElement('canvas');
+    return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+  } catch (x) {
+    return false;
+  }
+}
+
+var control = L.Routing.control({
+  waypoints: [
+    L.latLng(37.752, -122.418),
+    L.latLng(37.779, -122.391)
+  ],
+  // You can get your own Mapzen turn-by-turn & search API key from the Mapzen developer portal (https://mapzen.com/developers/)
+  geocoder: L.Control.Geocoder.mapzen('search-RH8pVLv'),
+  reverseWaypoints: true,
+  router: L.Routing.matrix('matrix-Yxnzyp9', {costing: 'auto'}),
+  formatter: new L.Routing.mapzenFormatter(),
+  summaryTemplate:'<div class="start">{name}</div><div class="info {costing}">{distance}, {time}</div>'
+}).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
