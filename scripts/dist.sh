a70c298b732cde2eb476ab861c26a86dee71cff2
Hanbyul Jo <hanbyul@nyu.edu>
2017-05-25 17:22:24 -0400

    set up scss

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -4,13 +4,14 @@ VERSION=`echo "console.log(require('./package.json').version)" | node`
 
 echo Building dist files for $VERSION...
 mkdir -p dist
-browserify -t browserify-shim src/* > dist/lrm-mapzen.js
-browserify -t uglifyify -t browserify-shim src/* | uglifyjs -c > dist/lrm-mapzen.min.js
+browserify -t browserify-shim src/*.js > dist/lrm-mapzen.js
+browserify -t uglifyify -t browserify-shim src/*.js | uglifyjs -c > dist/lrm-mapzen.min.js
 
-cp css/leaflet.routing.mapzen.css dist/lrm-mapzen.css
-cp css/lrm-mapzen-icons.svg dist/lrm-mapzen-icons.svg
-cp css/lrm-mapzen-modes.svg dist/lrm-mapzen-modes.svg
-cp css/lrm-mapzen-icons.png dist/lrm-mapzen-icons.png
-cp css/routing-icon.png dist/routing-icon.png
+node-sass --output-style compressed -o dist/ src/styles/lrm-mapzen.scss
+
+cp src/styles/lrm-mapzen-icons.svg dist/lrm-mapzen-icons.svg
+cp src/styles/lrm-mapzen-modes.svg dist/lrm-mapzen-modes.svg
+cp src/styles/lrm-mapzen-icons.png dist/lrm-mapzen-icons.png
+cp src/styles/routing-icon.png dist/routing-icon.png
 
 echo Done.
