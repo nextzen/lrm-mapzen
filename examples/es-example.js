13919581afe7e24558b8f42454f1979d88be9444
Hanbyul Jo <hanbyul@nyu.edu>
2017-10-04 13:16:51 -0400

    added es example

---
+++ examples/es-example.js
@@ -0,0 +1,27 @@
+var map = L.map('map');
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+
+var control = L.Routing.control({
+  waypoints: [
+    L.latLng(37.752, -122.418),
+    L.latLng(37.779, -122.391)
+  ],
+  // You can get your own Mapzen turn-by-turn & search API key from the Mapzen developer portal (https://mapzen.com/developers/)
+  geocoder: L.Control.Geocoder.mapzen('search-RH8pVLv'),
+  fitSelectedRoutes: true,
+  reverseWaypoints: true,
+  router: L.Routing.mapzen('valhalla-PVA4Y8g', {
+    costing: 'auto',
+    directions_options: {
+      language: 'es'
+    }
+  }),
+  formatter: new L.Routing.mapzenFormatter(),
+  summaryTemplate:'<div class="route-info {costing}">{distance}, {time}</div>'
+}).addTo(map);
+
+
+
+L.Routing.errorControl(control).addTo(map);
